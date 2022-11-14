f3c0b499bfefff15f13d0943bb7063eab6cad1d9
Janaina Trindade <janaina_tm_sap@hotmail.com>
2022-11-14 20:32:40 +0000

    creating tab bar

---
+++ mobdev-ca3/src/app/pages/tabs/tabs-routing.module.ts
@@ -0,0 +1,73 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { TabsPage } from './tabs.page';
+
+const routes: Routes = [
+  {
+    path: 'tabs',
+    component: TabsPage,
+    children: [
+      {
+        path: 'characters',
+        children: [
+          {
+            path: '',
+            loadChildren: () => import('../characters/characters.module').then(m => m.CharactersPageModule)
+      
+          },
+          {
+            path: ':id',
+            loadChildren: () => import('../character-details/character-details.module').then(m => m.CharacterDetailsPageModule)
+          }
+        ]
+      },
+      {
+        path: 'episodes',
+        children: [
+          {
+            path: '',
+            loadChildren: () => import('../episodes/episodes.module').then(m => m.EpisodesPageModule)
+      
+          },
+          {
+            path: ':id',
+            loadChildren: () => import('../episode-details/episode-details.module').then(m => m.EpisodeDetailsPageModule)
+          }
+        ]
+      },
+      {
+        path: 'deaths',
+        children: [
+          {
+            path: '',
+            loadChildren: () => import('../deaths/deaths.module').then(m => m.DeathsPageModule)
+      
+          }
+        ]
+      },
+      {
+        path: 'quotes',
+        children: [
+          {
+            path: '',
+            loadChildren: () => import('../quotes/quotes.module').then(m => m.QuotesPageModule)
+      
+          }
+        ]
+      }
+
+    ]
+  },
+  {
+    path: '',
+    redirectTo: '/tabs/characters',
+    pathMatch: 'full'
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class TabsPageRoutingModule {}
