84aac9823572f8c0fa5260cce236ef6be9174374
Janaina Trindade <janaina_tm_sap@hotmail.com>
2022-11-14 20:07:01 +0000

    creating pages

--- mobdev-ca3/src/app/app-routing.module.ts
+++ mobdev-ca3/src/app/app-routing.module.ts
@@ -11,6 +11,30 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'characters',
+    loadChildren: () => import('./pages/characters/characters.module').then( m => m.CharactersPageModule)
+  },
+  {
+    path: 'episodes',
+    loadChildren: () => import('./pages/episodes/episodes.module').then( m => m.EpisodesPageModule)
+  },
+  {
+    path: 'quotes',
+    loadChildren: () => import('./pages/quotes/quotes.module').then( m => m.QuotesPageModule)
+  },
+  {
+    path: 'deaths',
+    loadChildren: () => import('./pages/deaths/deaths.module').then( m => m.DeathsPageModule)
+  },
+  {
+    path: 'character-details',
+    loadChildren: () => import('./pages/character-details/character-details.module').then( m => m.CharacterDetailsPageModule)
+  },
+  {
+    path: 'episode-details',
+    loadChildren: () => import('./pages/episode-details/episode-details.module').then( m => m.EpisodeDetailsPageModule)
+  },
 ];
 
 @NgModule({
