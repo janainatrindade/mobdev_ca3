45eda5922b2d51efb0897635a48da729d99c910f
Janaina Trindade <janaina_tm_sap@hotmail.com>
2022-11-14 21:00:44 +0000

    update

--- mobdev-ca3/src/app/pages/characters/characters.page.ts
+++ mobdev-ca3/src/app/pages/characters/characters.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { ApiService } from '../../services/api.service';
 
 @Component({
   selector: 'app-characters',
@@ -7,9 +10,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CharactersPage implements OnInit {
 
-  constructor() { }
+  characters: Observable<any>;
+
+  constructor(private router: Router, private api: ApiService) { }
 
   ngOnInit() {
+      this.characters = this.api.getCharacters();
+      this.characters.subscribe(data => {
+      console.log('my characters: ', data);
+    });
   }
 
+  openDetails(character){
+      let characterId = character.char_id;
+      this.router.navigateByUrl(`/tabs/characters/${characterId}`);
+  }
 }
