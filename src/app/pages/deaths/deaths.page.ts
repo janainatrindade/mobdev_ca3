023096ab2c03e771be48a4a7886703ff51b4a309
Janaina Trindade <janaina_tm_sap@hotmail.com>
2022-11-15 22:38:32 +0000

    html character and deaths

--- src/app/pages/deaths/deaths.page.ts
+++ src/app/pages/deaths/deaths.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
+import { NavController } from '@ionic/angular';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-deaths',
   templateUrl: './deaths.page.html',
@@ -7,9 +10,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DeathsPage implements OnInit {
 
-  constructor() { }
+  deaths: Observable<any>;
+ 
+  constructor(private router: Router, private http: HttpClient) { }
 
   ngOnInit() {
+    this.deaths = this.http.get('https://www.breakingbadapi.com/api/deaths');
   }
+ 
 
 }
