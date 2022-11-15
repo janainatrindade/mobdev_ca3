4670170b3b9c2bd477c4040c1335e9ca3601bf82
Janaina Trindade <janaina_tm_sap@hotmail.com>
2022-11-15 22:09:22 +0000

    correcting error

--- src/app/pages/characters/characters-routing.module.ts
+++ src/app/pages/characters/characters-routing.module.ts
@@ -7,10 +7,6 @@ const routes: Routes = [
   {
     path: '',
     component: CharactersPage
-  },
-  {
-    path: 'characters',
-    loadChildren: () => import('./characters.module').then( m => m.CharactersPageModule)
   }
 ];
 
@@ -18,4 +14,4 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
 })
-export class FilmsPageRoutingModule {}
+export class CharactersPageRoutingModule {}
